e76ee8b72e33a1e642324d4e5aaf3b0bdf00b073
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-12 22:17:33 +0100

    Create layout.tsx

---
+++ src/app/success/layout.tsx
@@ -0,0 +1,9 @@
+import React, { Suspense } from "react";
+
+export default function SuccessLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {children}
+    </Suspense>
+  );
+}
